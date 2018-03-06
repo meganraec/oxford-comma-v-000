a7f08ccd68bdff1f2e79eaf742126bce72801b33
Megan Collier <meganraec@live.com>
2018-03-06 03:21:26 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 2
+  array.join(" and ")
+  elsif array.length >= 3
+  array.insert(-2, "and #{array.last}")
+  array.pop
+  array.join(", ")
+  else array.length == 1
+  array.join
+  end
 end
