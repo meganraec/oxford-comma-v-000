38cd972e67323fdd7c8beeffb06cacf8f8126023
Megan Collier <meganraec@live.com>
2018-03-06 03:12:57 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
-  array.insert(-2, "and #{array.last}")
+  # array.insert(-2, "and #{array.last}")
   array.pop
   if array.length >= 3
   array.join(", ")
-else array.join("and ")
+  else array.join("and ")
 end
 end
